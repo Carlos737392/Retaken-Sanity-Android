af2535eb4ace650c6ce75d17677e110221b47bab
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-23 09:59:51 +0200

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -121,6 +121,11 @@ class CreditsState extends MusicBeatState
 		bg.color = Std.parseInt(creditsStuff[curSelected][4]);
 		intendedColor = bg.color;
 		changeSelection();
+
+                #if android
+	        addVirtualPad(UP_DOWN, A_B);
+                #end
+
 		super.create();
 	}
 
